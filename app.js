4a17a1972855c7834f97f2fc3e8bfab404687fd8
shariqmehmood <78502043+shariqmehmood@users.noreply.github.com>
2021-07-18 15:49:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+
+            // Form validation
+
+function checkName() {
+     var name = document.getElementById("NameField");
+     if (name.value.length === 0) {
+     alert("Please enter your name");
+     name.focus();
+     name.style.background = "red";
+     
+
+
+ return false;
+}
+if (name.value.length < 3) {
+    alert("Please enter your ful name");
+    name.focus();
+    name.style.background = "green";
+    
+
+
+return false;
+}
+
+
+
+}
+
+
+function funn(){
+window.location.href="index.html"
+}
+
+
+
+
+function loadmomo() {
+document.getElementById("loadmo").style.display='inline-block' 
+}
+
+
+
+
+
+
+
+
+    
+
+
+
+ 
+
+    
+
+
+
+
+
+    
+    
+    
